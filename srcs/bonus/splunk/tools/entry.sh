afcde3fb23ec1219ee593b0de8c947a940f64000
ahkecha <ahkecha@users.noreply.github.com>
2022-09-24 17:09:48 +0100

    Deleted splunk

--- srcs/bonus/splunk/tools/entry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! grep -q PASSWORD /opt/splunk/etc/system/local/user-seed.conf ; then
-	echo "PASSWORD = $SPLUNK_PASSWD" >> /opt/splunk/etc/system/local/user-seed.conf
-fi
-
-/opt/splunk/bin/splunk start --accept-license --no-prompt > /dev/null 2>&1
-echo "ℹ️  Splunk Started at port 8000"
-tail -f /dev/null
