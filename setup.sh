cc76deb04217f78981daa5fc441ca220446ac073
ahkecha <ahkecha@users.noreply.github.com>
2022-10-12 14:11:33 -0400

    added cadvisor

--- setup.sh
+++ setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 # echo "Setting up volumes directories and appropriate permissions for ftp"
-if [ ! -f "./srcs/data/.success" ]; then
-	mkdir -p ./srcs/data
-	if [ ! -d "/tmp/mariadb" ]; then
-		mkdir -m 777 /tmp/mariadb
-	fi
-	if [ ! -d "/tmp/wordpress" ]; then
-		mkdir -m 777 /tmp/wordpress
-	fi
-	cd ./srcs/data
-	ln -s /tmp/mariadb ./srcs/data/mariadb > /dev/null 2>&1
-	ln -s /tmp/wordpress ./srcs/data/wordpress > /dev/null 2>&1
-	echo '1' > ./.success
+if [ ! -f "/home/ahkecha/data/.success" ]; then
+	mkdir -p /home/ahkecha/data/mariadb -m 777
+	mkdir -p /home/ahkecha/data/wordpress -m 777
+	mkdir -p /home/ahkecha/data/nginx -m 777
+	mkdir -p /home/ahkecha/data/adminer -m 777
+	mkdir -p /home/ahkecha/data/hugo -m 777
+	echo '1' > /home/ahkecha/data/.success
 fi
