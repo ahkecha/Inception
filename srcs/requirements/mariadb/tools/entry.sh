9772536979a4da0430cf8d290a5cbf36db4afec3
ahkecha <ahkecha@users.noreply.github.com>
2022-10-17 15:29:24 +0100

    ARG

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+
 cat << EOF > /usr/share/init.sql
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_RPASSWD}';
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-USE ${DB_NAME};
 CREATE USER IF NOT EXISTS ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* to ${DB_USER}@'%' WITH GRANT OPTION;
-UPDATE mysql.user SET Password = PASSWORD('${DB_RPASSWD}') WHERE User = 'root';
-UPDATE mysql.user SET plugin = '' WHERE user = 'root';
 FLUSH PRIVILEGES;
 EOF
 /etc/init.d/mysql start
